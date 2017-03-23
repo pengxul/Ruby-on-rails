c785841ce0e55eb97d3881ef820ad583c0806f65
peter <pengxl@yeah.net>
2017-03-23 22:45:48 +0800

    layout && test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'users/new'
 
-  resources :microposts
-  resources :users
+  root "static_pages#home"
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+
+  # edit route , enter /help jump to static_pages/help
+  # get  '/help', to: 'static_pages#help'
+  get  '/help',    to: 'static_pages#help' #, as: 'helf'
+  get  '/home',  to: 'static_pages#home'
+  get  '/about',  to: 'static_pages#about'
+  get  '/contact',  to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
